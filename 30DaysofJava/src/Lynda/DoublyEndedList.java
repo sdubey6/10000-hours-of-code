36daa0a4c46187d2b2d606810daa5b92d8d44efc
sdubey6 <sdubey6@uic.edu>
2018-02-11 15:46:12 -0600

    ++

--- 30DaysofJava/src/Lynda/DoublyEndedList.java
+++ 30DaysofJava/src/Lynda/DoublyEndedList.java
@@ -1,5 +1,38 @@
 package Lynda;
 
 public class DoublyEndedList {
+private Node head;
+private Node tail;
 
+public void insertAtTail(int data)
+{
+	Node newNode = new Node(data);
+	if (this.head == null) {
+		this.head = newNode;
+	 
+	}
+	if(this.tail != null)
+	{
+		this.tail= setNextNode(newNode);  //need to write steNextNode method
+		this.tail = newNode;
+		
+	}
+	
+	}
+
+@Override
+public String toString() {
+	String result = "{" ;
+	Node current = this.head;
+	while(current!= null) {
+		result += current.toString()+", ";
+		current = current.getNextNode();
+		
+		
+	}
+		result += "}";
+		return result;
+		
+			
+}
 }
---
+++ 30DaysofJava/src/Lynda/DoublyEndedListDemo.java
@@ -0,0 +1,17 @@
+package Lynda;
+
+public class DoublyEndedListDemo {
+public static void man (String [] args)
+{
+	DoublyEndedList dList = new DoublyEndedList ();
+	dList.insertAtTail(12);
+	dList.insertAtTail(13);
+	dList.insertAtTail(17);
+	
+	System.out.println(dList);
+	
+
+
+}
+
+}
