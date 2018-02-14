ec274f7a4e292d7f39e63d9f45ae6310cb21a437
sdubey6 <sdubey6@uic.edu>
2018-02-14 15:25:33 -0600

    +

---
+++ 30DaysofJava/src/CrackingTrhCodingInterview/OneAway.java
@@ -0,0 +1,19 @@
+package CrackingTrhCodingInterview;
+
+public class OneAway {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public boolean oneditaway{String s1, String s2){
+		char[] ch1 = s1.toCharArray();
+		char[] ch2 = s2.toCharArray();
+		
+		for (char c:ch1) {
+			
+		}
+	}
+
+}
